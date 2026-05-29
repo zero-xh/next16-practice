fb4f6ff42c87270cfba337a7a9c4ada43b5dc56b
zero-xh <93333954+zero-xh@users.noreply.github.com>
2026-05-29 22:43:20 +0800

    fix:修改next配置

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    cacheComponents: true,
-  },
+  cacheComponents: true,
   images: {
     remotePatterns: [
       {
